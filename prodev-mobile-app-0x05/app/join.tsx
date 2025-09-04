0a79bd0629aca6722bf0ff84bdf6b1f0dd069e64
Eliud Metto <eliudmett093@gmail.com>
2025-09-04 14:07:52 +0300

    New expo project with navigation

---
+++ prodev-mobile-app-0x05/app/join.tsx
@@ -0,0 +1,83 @@
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { styles } from "@/styles/_join";
+import { FACEBOOKLOGO, GOOGLELOGO, HEROLOGOGREEN } from "@/constants";
+import { useRouter } from "expo-router";
+
+export default function Index() {
+	const router = useRouter();
+
+	return (
+		<SafeAreaProvider>
+			<SafeAreaView style={styles.container}>
+				<View style={styles.iconsection}>
+					<Ionicons
+						name="arrow-back"
+						size={25}
+						onPress={() => router.navigate("/")}
+					/>
+					<Image source={HEROLOGOGREEN} />
+				</View>
+				<Text style={styles.titleText}>Create Your</Text>
+				<Text style={styles.titleText}>Account</Text>
+				<Text style={styles.subText}>
+					Enter your details to create new account
+				</Text>
+				<View style={styles.formGroup}>
+					<View>
+						<Text style={styles.formLabel}>Email</Text>
+						<TextInput
+							keyboardType="email-address"
+							style={styles.formControl}
+						/>
+					</View>
+					<View style={{ marginTop: 10 }}>
+						<Text style={styles.formLabel}>Password</Text>
+						<View style={styles.formPasswordControl}>
+							<TextInput style={styles.passwordControl} />
+							<FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+						</View>
+					</View>
+					<View style={{ marginTop: 10 }}>
+						<Text style={styles.formLabel}>Password</Text>
+						<View style={styles.formPasswordControl}>
+							<TextInput style={styles.passwordControl} />
+							<FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+						</View>
+					</View>
+				</View>
+				<TouchableOpacity style={styles.primaryButton}>
+					<Text style={styles.buttonText}>Create</Text>
+				</TouchableOpacity>
+				<View style={styles.dividerGroup}>
+					<View style={styles.divider}></View>
+					<Text style={styles.dividerText}>OR</Text>
+					<View style={styles.divider}></View>
+				</View>
+				<View style={styles.secondaryButtonGroup}>
+					<TouchableOpacity style={styles.secondaryButton}>
+						<View
+							style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+						>
+							<Image source={GOOGLELOGO} />
+							<Text style={styles.signupTitleText}>Continue with Google</Text>
+						</View>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.secondaryButton}>
+						<View
+							style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+						>
+							<Image source={FACEBOOKLOGO} />
+							<Text style={styles.signupTitleText}>Continue with Facebook</Text>
+						</View>
+					</TouchableOpacity>
+				</View>
+				<View style={styles.signupgroup}>
+					<Text style={styles.signupTitleText}>Don&#39;t have an account?</Text>
+					<Text style={styles.signupSubTitleText}>Sign in</Text>
+				</View>
+			</SafeAreaView>
+		</SafeAreaProvider>
+	);
+}
